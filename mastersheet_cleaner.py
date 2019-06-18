15dbc7c4421e3b3e19226998f065659a2711c73c
Daniel Chivvis <44475368+danielchivvis@users.noreply.github.com>
2019-06-18 15:06:50 -0400

    Create mastersheet_cleaner.py

---
+++ mastersheet_cleaner.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+col_master = pd.read_csv("Wolbach_Collection_Mastersheet_v1.csv")
+list(col_master.columns.values)
+
+col_master['MATERIAL'] = col_master['MATERIAL'].astype('|S')
+
+
+col_master = col_master[~col_master.MATERIAL.str.contains("CDROM")]
+col_master = col_master[~col_master.MATERIAL.str.contains("DATA")]
+col_master = col_master[~col_master.MATERIAL.str.contains("DVDRO")]
+col_master = col_master[~col_master.MATERIAL.str.contains("EQUIP")]
+col_master = col_master[~col_master.MATERIAL.str.contains("ISSBD")]
+col_master = col_master[~col_master.MATERIAL.str.contains("ISSLL")]
+col_master = col_master[~col_master.MATERIAL.str.contains("ISSUE")]
+col_master = col_master[~col_master.MATERIAL.str.contains("MAP")]
+col_master = col_master[~col_master.MATERIAL.str.contains("MEDIA")]
+col_master = col_master[~col_master.MATERIAL.str.contains("MFICH")]
+col_master = col_master[~col_master.MATERIAL.str.contains("MFILM")]
+col_master = col_master[~col_master.MATERIAL.str.contains("MIXED")]
+col_master = col_master[~col_master.MATERIAL.str.contains("RCASS")]
+col_master = col_master[~col_master.MATERIAL.str.contains("VCASS")]
+col_master = col_master[~col_master.MATERIAL.str.contains("VIS")]
+
+
+col_master.to_csv("Wolbach_Collection_Mastersheet_v2.csv")
+
+print('CSV saved')
