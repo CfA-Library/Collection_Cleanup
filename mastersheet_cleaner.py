8f11ea45021d1ddc76cb9f8f078a41461f88d1c5
Daniel Chivvis <44475368+danielchivvis@users.noreply.github.com>
2019-06-27 14:21:48 -0400

    Update mastersheet_cleaner.py

--- mastersheet_cleaner.py
+++ mastersheet_cleaner.py
@@ -5,7 +5,7 @@ col_master = pd.read_csv("Wolbach_Collection_Mastersheet_v1.csv", sep=',', error
 list(col_master.columns.values)
 
 col_master['MATERIAL'] = col_master['MATERIAL'].astype('|S')
-
+col_master['COLLECTION'] = col_master['COLLECTION'].astype('|S')
 
 col_master = col_master[~col_master.MATERIAL.str.contains("CDROM")]
 col_master = col_master[~col_master.MATERIAL.str.contains("DATA")]
@@ -21,9 +21,31 @@ col_master = col_master[~col_master.MATERIAL.str.contains("MFILM")]
 col_master = col_master[~col_master.MATERIAL.str.contains("MIXED")]
 col_master = col_master[~col_master.MATERIAL.str.contains("RCASS")]
 col_master = col_master[~col_master.MATERIAL.str.contains("VCASS")]
+col_master = col_master[~col_master.MATERIAL.str.contains("VDVD")]
 col_master = col_master[~col_master.MATERIAL.str.contains("VIS")]
 
+col_master = col_master[~col_master.COLLECTION.str.contains("HASTH")]
+col_master = col_master[~col_master.COLLECTION.str.contains("HDH")]
+col_master = col_master[~col_master.COLLECTION.str.contains("HDS")]
+col_master = col_master[~col_master.COLLECTION.str.contains("JRNLH")]
+col_master = col_master[~col_master.COLLECTION.str.contains("JRNLS")]
+col_master = col_master[~col_master.COLLECTION.str.contains("MFRMH")]
+col_master = col_master[~col_master.COLLECTION.str.contains("MFRMH")]
+col_master = col_master[~col_master.COLLECTION.str.contains("MFRMS")]
+col_master = col_master[~col_master.COLLECTION.str.contains("PCHTH")]
+col_master = col_master[~col_master.COLLECTION.str.contains("PCHTS")]
+col_master = col_master[~col_master.COLLECTION.str.contains("PORTH")]
+col_master = col_master[~col_master.COLLECTION.str.contains("PORTS")]
+col_master = col_master[~col_master.COLLECTION.str.contains("REFS")]
+col_master = col_master[~col_master.COLLECTION.str.contains("RESH")]
+col_master = col_master[~col_master.COLLECTION.str.contains("RESS")]
+col_master = col_master[~col_master.COLLECTION.str.contains("RREFS")]
+col_master = col_master[~col_master.COLLECTION.str.contains("SMAS")]
+col_master = col_master[~col_master.COLLECTION.str.contains("STORH")]
+col_master = col_master[~col_master.COLLECTION.str.contains("STORS")]
+col_master = col_master[~col_master.COLLECTION.str.contains("VIDH")]
+col_master = col_master[~col_master.COLLECTION.str.contains("WIAH")]
 
-col_master.to_csv("Wolbach_Collection_Mastersheet_v6.csv")
+col_master.to_csv("Wolbach_Collection_Mastersheet_v7.csv")
 
 print('CSV saved')
